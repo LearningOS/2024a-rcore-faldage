0c3060fd6b075db6d3821953ac8e340553ebaba9
faldage <faldage@outlook.com>
2024-11-14 15:39:43 +0000

    remove task from ready_queue when scheduling

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -1,14 +1,13 @@
 //!Implementation of [`TaskManager`]
-use core::cmp::min;
 
 use super::TaskControlBlock;
 use crate::sync::UPSafeCell;
-use alloc::collections::VecDeque;
 use alloc::sync::Arc;
+use alloc::vec::Vec;
 use lazy_static::*;
 ///A array of `TaskControlBlock` that is thread-safe
 pub struct TaskManager {
-    ready_queue: VecDeque<Arc<TaskControlBlock>>,
+    ready_queue: Vec<Arc<TaskControlBlock>>,
 }
 
 /// A simple FIFO scheduler.
@@ -16,33 +15,27 @@ impl TaskManager {
     ///Creat an empty TaskManager
     pub fn new() -> Self {
         Self {
-            ready_queue: VecDeque::new(),
+            ready_queue: Vec::new(),
         }
     }
     /// Add process back to ready queue
     pub fn add(&mut self, task: Arc<TaskControlBlock>) {
-        self.ready_queue.push_back(task);
+        self.ready_queue.push(task);
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
         //self.ready_queue.pop_front()
-        let mut min_stride = self
+        let min_stride = self
             .ready_queue
-            .front()
-            .unwrap()
-            .inner_exclusive_access()
-            .get_stride();
-        for t in &self.ready_queue {
-            min_stride = min(min_stride, t.inner_exclusive_access().get_stride());
-        }
-        for t in &self.ready_queue {
-            let mut inner = t.inner_exclusive_access();
-            if inner.get_stride() == min_stride {
-                inner.add_stride();
-                return Some(t.clone());
-            }
-        }
-        None
+            .iter()
+            .map(|e| e.inner_exclusive_access().get_stride())
+            .min()?;
+        let index = self
+            .ready_queue
+            .iter()
+            .position(|e| e.inner_exclusive_access().get_stride() == min_stride)
+            .unwrap();
+        Some(self.ready_queue.remove(index))
     }
 }
 
